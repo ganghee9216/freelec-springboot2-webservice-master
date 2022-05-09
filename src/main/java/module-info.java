bbe60bb89ca003780bcf93aa0cf27550f73f9972
KimKyungH <qoemfqhdl@gmail.com>
2022-05-09 22:57:18 +0900

    테스트 코드 작성

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.freelecspringboot2webservicemaster {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.freelecspringboot2webservicemaster to javafx.fxml;
-    exports com.example.freelecspringboot2webservicemaster;
-}
